7d62f09597f81bc31264409e4f8bda5fb9e9d0f0
Paul Ushiro <ushiropaul2007@gmail.com>
2025-06-01 23:48:29 -0300

    2nd commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
+
+
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '4rem' }}>
       <h1>Bienvenido al Sistema Escolar</h1>
-      <p>Por favor, <a href="/login">inicie sesión</a> para continuar.</p>
+      <Link to='/login'>inicia sesión</Link>
     </div>
   );
 }
