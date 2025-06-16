26da2f4613dc39d15137f867833a2c5455ebcf27
Paul Ushiro <ushiropaul2007@gmail.com>
2025-06-16 17:36:07 -0300

    probando arreglo en panel

--- src/pages/Padres/Panel.jsx
+++ src/pages/Padres/Panel.jsx
@@ -43,7 +43,7 @@ export default function Panel() {
             cuatrimestre,
             curso_materia (
               id,
-              materia (
+              materias (
                 nombre
               )
             )
