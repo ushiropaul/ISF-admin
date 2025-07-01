a81433ecb9cb08bec257409e98c135994223288d
Paul Ushiro <ushiropaul2007@gmail.com>
2025-07-01 17:57:22 -0300

    f

--- netlify/functions/notify-falta-email.ts
+++ netlify/functions/notify-falta-email.ts
@@ -2,8 +2,8 @@ import { Handler } from '@netlify/functions';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.VITE_SUPABASE_URL!,
+  process.env.VITE_SUPABASE_ANON_KEY!
 );
 
 export const handler: Handler = async (event, context) => {
